f3ac31d1bb49e000d469ba5811669b70f034b221
Pvl <impetus@yandex.ru>
2019-04-26 11:01:27 +0300

    Authentication

---
+++ App/AuthForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class AuthForm : Form
+    {
+
+        Form reg;
+
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            reg = new RegForm();
+            reg.Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(Properties.Settings.Default.dbConnectionSettings);
+            connection.Open();
+            String login = textBox1.Text;
+            String pass = textBox2.Text;
+
+            SqlCommand command = new SqlCommand("SELECT * FROM users WHERE login = '" + login + "' AND password = '" + pass + "'", connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            String role = "", name ="";
+            while(reader.Read())
+            {
+               
+                role = reader[2].ToString();
+                name = reader[3].ToString();
+            }
+
+            Form form = null;
+            switch (role)
+            {
+                case "User":
+                    form = new UserForm();                   
+                    break;
+                case "Director":
+                    form = new AdminForm();               
+                    break;
+                case "Ware":
+                    form = new WareForm();     
+                    break;
+                case "Manager":
+                    form = new ManagerForm();                 
+                    break;                                          
+            }           
+            this.Hide();
+            form.Show();
+
+
+
+
+        }
+    }
+}
---
+++ App/RegForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class RegForm : Form
+    {
+        SqlConnection connection = new SqlConnection(Properties.Settings.Default.dbConnectionSettings);
+        Form auth; // null
+
+        public RegForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int errors = 0;
+            String message = "";
+
+            if(textBox1.Text == "")
+            {
+                errors++;
+                message += "Пожалуйста введите имя\n";
+            }
+            if (textBox2.Text == "")
+            {
+                errors++;
+                message += "Пожалуйста введите логин\n";
+            }
+            if (textBox3.Text == "")
+            {
+                errors++;
+                message += "Пожалуйста введите пароль\n";
+            }
+
+            if (textBox4.Text == "")
+            {
+                errors++;
+                message += "Пожалуйста введите подтверждение пароля\n";
+            }
+            
+            if(textBox3.Text != textBox4.Text)
+            {
+                errors++;
+                message += "Пароли не совпадают!\n";
+            }
+
+            if(errors>0) {
+                MessageBox.Show(message);
+            } else {
+                connection.Open();
+                try
+                {
+                    SqlCommand command = new SqlCommand("INSERT INTO users (login,password,role,name) VALUES (@login,@password,@role,@name)",connection);
+                    command.Parameters.AddWithValue("@login", textBox2.Text);
+                    command.Parameters.AddWithValue("@password", textBox3.Text);
+                    command.Parameters.AddWithValue("@role", "User");
+                    command.Parameters.AddWithValue("@name", textBox1.Text);
+                    int regged = Convert.ToInt32(command.ExecuteNonQuery());
+                    connection.Close();
+                    MessageBox.Show("Пользователь успешно зарегистрировался!\n");
+                } catch
+                {
+                    MessageBox.Show("Такой пользователь существует!\n");
+
+                }
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            auth = new AuthForm();
+            auth.Show();
+        }
+    }
+}
