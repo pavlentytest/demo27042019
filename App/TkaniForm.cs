33ef65dec374780a119a5faf1b647f35f90f8c43
Pvl <impetus@yandex.ru>
2019-04-29 11:38:04 +0300

    Add menu for wareform & tkani form

--- App/TkaniForm.cs
+++ App/TkaniForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,31 @@ namespace App
            DataSet ds = new DataSet();
            sda.Fill(ds, "tkani");
            dataGridView1.DataSource = ds.Tables["tkani"];
+
+           DataGridViewImageColumn img = new DataGridViewImageColumn();
+            img.Name = "img";
+            img.HeaderText = "Картинка";
+            dataGridView1.Columns.Add(img);
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[1].Value != null)
+                {
+                    string basePath = "C:/Users/lickett2019/source/repos/App/App/images/tkani/";
+                    string filename = dataGridView1.Rows[i].Cells[1].Value.ToString() + ".jpg";
+                    string fullPath = basePath + filename;
+                    Image image;
+                    if (File.Exists(fullPath))
+                    {
+                        image = Image.FromFile(fullPath);
+                    }
+                    else
+                    {
+                        image = Image.FromFile(basePath+"empty.jpg");
+                    }
+                    dataGridView1.Rows[i].Cells["img"].Value = image;
+                }
+            }
+           
         }
 
         private void button1_Click(object sender, EventArgs e)
