bf60487f4c73de8dfe537fff1c5cfcb1d6ade12a
Pvl <impetus@yandex.ru>
2019-05-14 08:31:50 +0300

    Combobox for counter

--- App/UserOrderForm.Designer.cs
+++ App/UserOrderForm.Designer.cs
@@ -31,20 +31,20 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.test2DataSet3 = new App.test2DataSet3();
             this.izdelieBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.test2DataSet3 = new App.test2DataSet3();
             this.izdelieTableAdapter = new App.test2DataSet3TableAdapters.izdelieTableAdapter();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.test2DataSet3)).BeginInit();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.izdelieBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.test2DataSet3)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -69,15 +69,15 @@
             this.comboBox1.ValueMember = "ID";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // test2DataSet3
-            // 
-            this.test2DataSet3.DataSetName = "test2DataSet3";
-            this.test2DataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
             // izdelieBindingSource
             // 
             this.izdelieBindingSource.DataMember = "izdelie";
             this.izdelieBindingSource.DataSource = this.test2DataSet3;
+            // 
+            // test2DataSet3
+            // 
+            this.test2DataSet3.DataSetName = "test2DataSet3";
+            this.test2DataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // izdelieTableAdapter
             // 
@@ -100,16 +100,6 @@
             this.label3.Size = new System.Drawing.Size(66, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Количество";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(226, 87);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(77, 20);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "1";
-            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
-            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // button1
             // 
@@ -166,18 +156,27 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(226, 87);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(63, 21);
+            this.comboBox2.TabIndex = 12;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
             // UserOrderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(758, 462);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
@@ -185,8 +184,8 @@
             this.Name = "UserOrderForm";
             this.Text = "UserOrderForm";
             this.Load += new System.EventHandler(this.UserOrderForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.test2DataSet3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.izdelieBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.test2DataSet3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,12 +200,12 @@
         private test2DataSet3TableAdapters.izdelieTableAdapter izdelieTableAdapter;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
